d97df6d538fe86355dd7158400c3ba5c89e12012
Jose Vinicius <joseviniciuus@outlook.com>
2019-08-29 20:19:06 -0300

    teste

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const helmet = require('helmet');
 const path = require('path');
 const fs = require('fs');
+const jsonfile = require('jsonfile');
 
 const fileDir = path.resolve('data.json');
 
@@ -12,8 +13,6 @@ if (!fs.existsSync(fileDir)) {
     console.log('CREATE DATA FILE:', fileDir);
     fs.writeFileSync(fileDir, JSON.stringify({ clientes: [] }));
 }
-
-const jsonfile = require('jsonfile');
 
 console.log('DATA:', fileDir);
 
@@ -26,16 +25,40 @@ app.get('/', (req, res) => {
 });
 
 app.get('/clientes', async(req, res) => {
-    const clientes = await jsonfile.readFile(fileDir);
-    return res.send(clientes);
+    try {
+        const clientes = await jsonfile.readFile(fileDir);
+        return res.send(clientes);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
 });
 
-app.post('/clientes', (req, res) => {
-    return res.status(200).send();
+app.post('/clientes', async(req, res) => {
+    try {
+        let { body } = req;
+        let response = await jsonfile.readFile(fileDir);
+        body.id = Math.random().toString(36).substr(2, 9);
+        body.dtTransacao = new Date().toISOString();
+        response.clientes.reverse();
+        response.clientes.push(body);
+        response.clientes.reverse();
+        jsonfile.writeFile(fileDir, response);
+        return res.status(200).send(body);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
 });
 
-app.delete('/clientes', (req, res) => {
-    return res.status(200).send();
+app.delete('/clientes', async(req, res) => {
+    try {
+        let { id } = req.body;
+        let response = await jsonfile.readFile(fileDir);
+        response.clientes = response.clientes.filter((cliente) => cliente.id !== id);
+        jsonfile.writeFile(fileDir, response);
+        return res.status(200).send();
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
 });
 
 const porta = process.env.PORT || 3001;
